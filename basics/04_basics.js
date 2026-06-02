ebe37e4c3a763a39b3d4cc9ca8f60a03a534decc
ROHIT KUMAR <91519108+222000rohitkumar@users.noreply.github.com>
2026-06-02 16:35:11 +0530

    Add files via upload

---
+++ basics/04_basics.js
@@ -0,0 +1,13 @@
+// +++++++++++++Maths+++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+console.log(Math.floor(4.5));
+console.log(Math.ceil(4.2));
+console.log(Math.max(1, 2, 3, 4, 5));
+console.log(Math.min(1, 2, 3, 4, 5));
+console.log(Math.random());
+console.log(Math.random() * 100);
+console.log(Math.floor(Math.random() * 100));
+console.log(Math.floor(Math.random() * (100 - 1 + 1)) + 1);
